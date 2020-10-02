eb748aa0c986d10395d7c2dfa98e4b2dc82b620e
Kristin Obermyer <ms-O@users.noreply.github.com>
2020-10-02 15:13:51 -0400

    Update siteConfig.js

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -12,8 +12,8 @@
 const users = [];
 
 const siteConfig = {
-    title: 'Your Name Here', // Title for your website.
-    tagline: 'Continuously improving, aspiring technologist.',
+    title: 'Kristin Obermyer', // Title for your website.
+    tagline: 'Curious, experienced technologist.',
     profilePhoto: 'img/profile.png',
     url: 'https://joatmon08.github.io', // Your website URL
     baseUrl: '/tech-portfolio/', // Base URL for your project */
@@ -23,7 +23,7 @@ const siteConfig = {
 
     // Used for publishing and more
     projectName: 'tech-portfolio',
-    organizationName: 'joatmon08',
+    organizationName: 'ms-O',
     // For top-level user or org sites, the organization is still the same.
     // e.g., for the https://JoelMarcey.github.io site, it would be set like...
     //   organizationName: 'JoelMarcey'
